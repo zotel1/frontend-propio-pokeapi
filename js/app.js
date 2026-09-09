b4f49f100ca66c9c8e179a31b84f72d430af604d
zotel1 <zotelsigel@gmail.com>
2026-09-09 10:06:23 -0300

    agregamos los estilos de los componentes y el responsive y una prueba de como se veria nuestra app

--- js/app.js
+++ js/app.js
@@ -1,8 +1,60 @@
 const screenContent = document.querySelector('#screen-content')
 
 screenContent.innerHTML = `
-  <div>
-    <h2>Pokédex — ISFD</h2>
-    <p>Frontend funcionando correctamente.</p>
-  </div>
+  <section class="pokemon-display">
+
+    <div class="pokemon-display__image-wrapper">
+      <img
+        class="pokemon-display__image"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+        alt="Pikachu"
+      >
+    </div>
+
+    <p class="pokemon-display__id">
+      #025
+    </p>
+
+    <h2 class="pokemon-display__name">
+      Pikachu
+    </h2>
+
+    <div class="pokemon-display__types">
+      <span class="type-chip type-chip--electric">
+        Electric
+      </span>
+    </div>
+
+    <div class="pokemon-stats">
+
+      <div class="pokemon-stat">
+        <span class="pokemon-stat__label">
+          Peso
+        </span>
+
+        <span class="pokemon-stat__value">
+          6 kg
+        </span>
+      </div>
+
+      <div class="pokemon-stat">
+        <span class="pokemon-stat__label">
+          Altura
+        </span>
+
+        <span class="pokemon-stat__value">
+          0.4 m
+        </span>
+      </div>
+
+    </div>
+
+    <button
+      class="screen-button"
+      type="button"
+    >
+      + Agregar a colección
+    </button>
+
+  </section>
 `
